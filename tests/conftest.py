dd00e3bcd9860877aa0c4120159a0f573ea4e771
John Hunter <2665766+JNKHunter@users.noreply.github.com>
2023-11-05 00:34:05 -0400

    fixed issue with pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,12 +6,8 @@ from src.app import create_app
 def app():
 	
 	app = create_app()
-	db = setup_database(app)
-	migrate = Migrate(app,db)
-	print("HEEEEELLLLOOOO")
-	yield app
+	return app
 
 @pytest.fixture()
 def client(app):
-	print("HEEEEELLLLOOOO")
 	return app.test_client()
