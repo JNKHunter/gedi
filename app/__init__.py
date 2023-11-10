448a8a1e5c65c9d2e63641da207f9d326ef0e49f
John Hunter <2665766+JNKHunter@users.noreply.github.com>
2023-11-09 21:54:12 -0500

    added migration to flask app

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from app.database import db
 from app import routes
+from flask_migrate import Migrate
 import os
 
 database_uri = os.getenv('DATABASE_URI')
@@ -15,5 +16,6 @@ def create_app(database_uri=None):
         app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
 
     db.init_app(app)
+    migrate = Migrate(app, db)
     app.register_blueprint(routes.bp)
     return app
